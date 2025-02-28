235ef0a73ebe989423ae6c4699c5b1e980fc233a
Ayush Singh Rathore <singhrathoreayush824@gmail.com>
2025-02-28 20:04:51 +0530

    Update database.js

--- Backend/src/db/database.js
+++ Backend/src/db/database.js
@@ -1,13 +1,11 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-// Connect to the SQLite database
 const db = new sqlite3.Database(path.join(__dirname, "../../../database/users.db"), (err) => {
     if (err) console.error(err.message);
     else console.log("Connected to the SQLite database.");
 });
 
-// Create Tables
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS users (
